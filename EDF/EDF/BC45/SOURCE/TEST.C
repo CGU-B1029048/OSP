c993a8f417bb5d38bbd53fae69c561acc4cbbdd5
Austin1153 <austinlee0310@gmail.com>
2024-06-12 15:17:25 +0800

    EDF new

--- EDF/EDF/BC45/SOURCE/TEST.C
+++ EDF/EDF/BC45/SOURCE/TEST.C
@@ -82,16 +82,12 @@ TASK_EXTRA_DATA  TaskExtraData[8];
 INT8U NumberOfTasks; 
 INT8U ExecutionTime[8]; 
 INT8U PeriodTime[8]; 
-// INT8U *exe;
-// INT8U *period;
-INT8U *TaskList;
-INT8U *TempPeriodTime;
-// INT8U *priority;
 INT32U MyStartTime;
-INT32U task_display_counter = 2;
-/*******************************************************************************************************/
 
 
+INT8U *TaskOrder;
+INT32U task_display_x = 2;
+INT8U task_display_y = 18;/*******************************************************************************************************/
 /*
 *********************************************************************************************************
 *                                         FUNCTION PROTOTYPES
@@ -117,18 +113,34 @@ static  void  TaskStartDisp(void);
 *                                                  MAIN
 *********************************************************************************************************
 */
-//bool checkSort(INT8U *PeriodTime,INT8U size)
-//{
-//	INT8U i
-//	for(i=1;i<size;i++)
-//	{
-//		if(PeriodTime[i-1]>PeriodTime[i])
-//		{
-//			return true;
-//		}
-//	}
-//	return false;
-//}
+
+// Sorting for Task with their period, to make RM
+void BubbleSort(TASK_EXTRA_DATA *array, int *order, int Num_of_Tasks) {
+    int i, j;//, ttmmpp;
+    for (i = 0; i <= Num_of_Tasks; i++) {
+        for (j = i; j < Num_of_Tasks; j++) {
+            if (array[i].Period > array[j].Period) {
+                TASK_EXTRA_DATA tmp = array[i];
+                array[i] = array [j];
+                array[j] = tmp;
+                // Sort task order 
+                // ttmmpp = order[i];
+                // order[i] = order[j];
+                // order[j] = ttmmpp;
+            }
+        }
+    }
+}
+
+// sort exe and period time according to task ID
+void InsertExePeriodTime(INT8U *Exe, INT8U *per, int Num_of_Tasks) {
+    int i;
+    for (i = 0; i < Num_of_Tasks; i++) {
+        Exe[TaskExtraData[i].TaskID-1] = TaskExtraData[i].ExecutionTime / OS_TICKS_PER_SEC;
+        per[TaskExtraData[i].TaskID-1] = TaskExtraData[i].Period / OS_TICKS_PER_SEC;
+    }
+}
+
 
 
 void main (void){
@@ -144,9 +156,8 @@ void main (void){
     INT8U j;
     InputFile = fopen("Input1.txt","r");
     fscanf(InputFile, "%d", &NumberOfTasks);
-    // period = (int*)malloc(sizeof(int)*NumberOfTasks);
-    // exe = (int*)malloc(sizeof(int)*NumberOfTasks);
-    TaskList = (int*)malloc(sizeof(int)*NumberOfTasks);
+
+    TaskOrder = (int *)malloc(sizeof(int) * NumberOfTasks);
 	// priority = (int*)malloc(sizeof(int)*NumberOfTasks);
     for(i=0; i< NumberOfTasks; i++)
     {
@@ -157,29 +168,11 @@ void main (void){
         TaskExtraData[i].Deadline = PeriodTime[i] * OS_TICKS_PER_SEC; 
         TaskExtraData[i].RemainTime = ExecutionTime[i] * OS_TICKS_PER_SEC;
         TaskExtraData[i].TaskID=i+1;
-        TaskList[i] = i; 
-        // exe[i]=ExecutionTime[i];
-        // period[i]=PeriodTime[i];
     }
     fclose(InputFile);
 
-// toby : order task
-
-//     for (i = 0; i < NumberOfTasks - 1; i++){
-//         for (j = 0; j < NumberOfTasks - 1 - i; j++){
-//             if (PeriodTime[j] > PeriodTime[j + 1]){
-//                 temp = PeriodTime[j];
-//                 PeriodTime[j] = PeriodTime[j + 1];
-//                 PeriodTime[j + 1] = temp;
-//                 temp = TaskList[j];
-//                 TaskList[j] = TaskList[j + 1];
-//                 TaskList[j + 1] = temp;
-//             }
-//         }
-//	 }
-	
-	  selectionSort(PeriodTime,TaskList,NumberOfTasks);
-
+    BubbleSort(TaskExtraData, TaskOrder, NumberOfTasks);
+    InsertExePeriodTime(ExecutionTime, PeriodTime, NumberOfTasks);
 
 /*******************************************************************************************************/
 
@@ -215,49 +208,49 @@ void main (void){
 *                                               STARTUP TASK
 *********************************************************************************************************
 */
-void swap(INT8U* a,INT8U *b){
-    INT8U temp = *a;
-    *a = *b;
-    *b = temp;
-}
-// sam: quick sort 
-void quickSort (INT8U *PeriodTime , INT8U *TaskList,const INT8U left, const INT8U right){
-    if (left<right ){
-        INT8U i=left,j=right+1,pivot=PeriodTime[left];
-        INT8U temp;
-        do{
-            do i++; while( PeriodTime[i]<pivot ); 
-            do j--; while( PeriodTime[j]>pivot ); 
-            // swap( ptr[i],ptr[j] )
-             if ( i<j ){
-                swap(&PeriodTime[i],&PeriodTime[j]);
-                swap(&TaskList[i],&TaskList[j]);
-             }
-        }while( i<j );
-        swap(&PeriodTime[left],&PeriodTime[j]);
-        swap(&TaskList[left],&TaskList[j]);
-        temp=1;
-        quickSort(PeriodTime,TaskList,left,j-1 );
-        quickSort(PeriodTime,TaskList,j+1,right );
-    }
-}
-void selectionSort(INT8U *PeriodTime , INT8U *TaskList, INT8U size){
-    INT8U i, j, min_idx, temp;
-    for (i=0; i<size-1; i++) {
-        min_idx = i;
-        for (j=i+1; j<size; j++) {
-            if (PeriodTime[j] < PeriodTime[min_idx]) {
-                min_idx = j;
-            }
-        }
-        // swap
-        // temp = PeriodTime[min_idx];
-        // PeriodTime[min_idx] = PeriodTime[i];
-        // PeriodTime[i] = temp;
-        swap(&PeriodTime[min_idx],&PeriodTime[i]);
-        swap(&TaskList[min_idx],&TaskList[i]);
-    }
-}
+// void swap(INT8U* a,INT8U *b){
+//     INT8U temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+// // sam: quick sort 
+// void quickSort (INT8U *PeriodTime , INT8U *TaskList,const INT8U left, const INT8U right){
+//     if (left<right ){
+//         INT8U i=left,j=right+1,pivot=PeriodTime[left];
+//         INT8U temp;
+//         do{
+//             do i++; while( PeriodTime[i]<pivot ); 
+//             do j--; while( PeriodTime[j]>pivot ); 
+//             // swap( ptr[i],ptr[j] )
+//              if ( i<j ){
+//                 swap(&PeriodTime[i],&PeriodTime[j]);
+//                 swap(&TaskList[i],&TaskList[j]);
+//              }
+//         }while( i<j );
+//         swap(&PeriodTime[left],&PeriodTime[j]);
+//         swap(&TaskList[left],&TaskList[j]);
+//         temp=1;
+//         quickSort(PeriodTime,TaskList,left,j-1 );
+//         quickSort(PeriodTime,TaskList,j+1,right );
+//     }
+// }
+// void selectionSort(INT8U *PeriodTime , INT8U *TaskList, INT8U size){
+//     INT8U i, j, min_idx, temp;
+//     for (i=0; i<size-1; i++) {
+//         min_idx = i;
+//         for (j=i+1; j<size; j++) {
+//             if (PeriodTime[j] < PeriodTime[min_idx]) {
+//                 min_idx = j;
+//             }
+//         }
+//         // swap
+//         // temp = PeriodTime[min_idx];
+//         // PeriodTime[min_idx] = PeriodTime[i];
+//         // PeriodTime[i] = temp;
+//         swap(&PeriodTime[min_idx],&PeriodTime[i]);
+//         swap(&TaskList[min_idx],&TaskList[i]);
+//     }
+// }
 void  TaskStart (void *pdata)
 {
 #if OS_CRITICAL_METHOD == 3                                /* Allocate storage for CPU status register */
@@ -321,10 +314,7 @@ static  void  TaskStartDispInit (void)
     PC_DispStr( 0,  9, "-------------  ----------  ----------  ----------  ----------  ----------  ---- ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     for(i=0; i< NumberOfTasks; i++)
     {
-        // sprintf(s,"Task%1d()    :                                       %2d          %2d               ", i+1, period[i], exe[i]);
-        INT8U p_time = TaskExtraData[i].Period/OS_TICKS_PER_SEC;
-        INT8U e_time = TaskExtraData[i].ExecutionTime/OS_TICKS_PER_SEC;
-        sprintf(s,"Task%1d()    :                                       %2d          %2d               ", TaskExtraData[i].TaskID,p_time , e_time);
+        sprintf(s,"Task%1d()    :                                         %2d       %2d                ",i+1,PeriodTime[i],ExecutionTime[i]);
         PC_DispStr(0, 10+i, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     }
     for(j=(NumberOfTasks+10); j<22; j++)
@@ -417,15 +407,15 @@ static  void  TaskStartCreateTasks (void)
         //                TASK_STK_SIZE,
         //                &TaskExtraData[i],
         //                OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-        taskID = TaskList[i];
+        // taskID = TaskList[i];
         OSTaskCreateExt(PeriodicTask,
                        (void *)0,
-                       &TaskStk[taskID][TASK_STK_SIZE-1],
+                       &TaskStk[i][TASK_STK_SIZE-1],
                        (PERIODIC_TASK_START_PRIO + i),
-                       (PERIODIC_TASK_START_ID + taskID),
-                       &TaskStk[taskID][0],
+                       (PERIODIC_TASK_START_ID + i),
+                       &TaskStk[i][0],
                        TASK_STK_SIZE,
-                       &TaskExtraData[taskID],
+                       &TaskExtraData[i],
                        OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 /*
         sprintf(s,"           Task%1d is created with piority %1d                                   ", i+1 , PERIODIC_TASK_START_PRIO + i);
@@ -471,7 +461,8 @@ void  PeriodicTask (void *pdata)
     char p[34];
     INT8U i;
     INT32U TaskTime;
-    
+    INT8U CurTskPrio;
+
     pdata=pdata;
     MyPtr = OSTCBCur->OSTCBExtPtr;
     x =0;
@@ -482,18 +473,21 @@ void  PeriodicTask (void *pdata)
     for (;;){
 //         toby, display running task to screen
         sprintf(s, "%d->",MyPtr->TaskID);
-        PC_DispStr( task_display_counter,  18, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-        task_display_counter = task_display_counter+3;
-        //sprintf(s, "switch at %10d",OSTimeGet()/OS_TICKS_PER_SEC-1);
-        //PC_DispStr( 0,  20, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        PC_DispStr( task_display_x,  task_display_y, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        task_display_x = task_display_x + 3;
+        if (task_display_x > 77) {
+            task_display_x = 2;
+            task_display_y++;
+        }
 
 	    x++;
+        CurTskPrio = OSPrioCur - PERIODIC_TASK_START_PRIO;
 	    sprintf(s, "%4d",x); 
-        PC_DispStr(75, 10 + TaskList[OSPrioCur - PERIODIC_TASK_START_PRIO], s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        PC_DispStr(75, 10 + TaskExtraData[CurTskPrio].TaskID-1, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
 	    sprintf(s, "%10d ",OSTimeGet()/OS_TICKS_PER_SEC-1); 
-        PC_DispStr(15, 10 + TaskList[OSPrioCur - PERIODIC_TASK_START_PRIO], s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY); 
+        PC_DispStr(15, 10 + TaskExtraData[CurTskPrio].TaskID-1, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY); 
 	    sprintf(s, "%10d ", MyPtr->Deadline/OS_TICKS_PER_SEC-1); 
-        PC_DispStr(39, 10 + TaskList[OSPrioCur - PERIODIC_TASK_START_PRIO], s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        PC_DispStr(39, 10 + TaskExtraData[CurTskPrio].TaskID-1, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
         while(1){
             if(MyPtr->RemainTime <=0) {break;}
         }
@@ -502,7 +496,7 @@ void  PeriodicTask (void *pdata)
         MyPtr->RemainTime = MyPtr->ExecutionTime;
 
 	    sprintf(s, "%10d ",OSTimeGet()/OS_TICKS_PER_SEC-1); 
-        PC_DispStr(27, 10 + TaskList[OSPrioCur - PERIODIC_TASK_START_PRIO], s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        PC_DispStr(27, 10 + TaskExtraData[CurTskPrio].TaskID-1, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
 
         if(MyPtr->Deadline - MyPtr->Period > OSTimeGet()){
             OSTimeDly(MyPtr->Deadline - MyPtr->Period - OSTimeGet());
